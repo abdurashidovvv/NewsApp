dd25ba24c8114f0dddc493a6710107a55a1957c5
Saydullo <abdurashidovsaydullo35@gmail.com>
2023-04-11 16:54:08 +0500

    Category Adapter

---
+++ app/src/main/java/uz/ilhomjon/newsapp/view/adapters/CategoryAdapter.kt
@@ -0,0 +1,39 @@
+package uz.ilhomjon.newsapp.view.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import uz.ilhomjon.newsapp.R
+import uz.ilhomjon.newsapp.database.entity.AllCategory
+import uz.ilhomjon.newsapp.databinding.CategoryRvItemBinding
+
+class CategoryAdapter(val list: ArrayList<AllCategory>, val categoryItemCLick: CategoryItemCLick) :
+    RecyclerView.Adapter<CategoryAdapter.Vh>() {
+
+    inner class Vh(private val rvItem: CategoryRvItemBinding) : ViewHolder(rvItem.root) {
+        fun onBind(allCategory: AllCategory, position: Int) {
+            rvItem.tv.text=allCategory.category_name
+            if (allCategory.isSelected){
+                rvItem.checkImage.setImageResource(R.drawable.check)
+            }
+            rvItem.checkbox.setOnClickListener {
+                categoryItemCLick.onClick(allCategory, position)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Vh {
+        return Vh(CategoryRvItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: Vh, position: Int) {
+        holder.onBind(list[position], position)
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    interface CategoryItemCLick{
+        fun onClick(allCategory: AllCategory, position: Int)
+    }
+}
--- app/src/main/java/uz/ilhomjon/newsapp/view/ui/register/ThirdFragment.kt
+++ app/src/main/java/uz/ilhomjon/newsapp/view/ui/register/ThirdFragment.kt
@@ -6,55 +6,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import uz.ilhomjon.newsapp.R
+import uz.ilhomjon.newsapp.database.entity.AllCategory
+import uz.ilhomjon.newsapp.databinding.FragmentThirdBinding
+import uz.ilhomjon.newsapp.utils.Constants
+import uz.ilhomjon.newsapp.view.adapters.CategoryAdapter
+import uz.ilhomjon.newsapp.viewmodel.DatabaseViewModel
+import javax.inject.Inject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ThirdFragment : Fragment(), CategoryAdapter.CategoryItemCLick {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ThirdFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ThirdFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    @Inject
+    lateinit var databaseViewModel: DatabaseViewModel
+    private val binding by lazy { FragmentThirdBinding.inflate(layoutInflater) }
+    private lateinit var categoryAdapter: CategoryAdapter
+    private lateinit var list: ArrayList<AllCategory>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false)
+    ): View {
+        list = ArrayList()
+        val allCategory = Constants.categoryList
+        for (item in allCategory) {
+            val category = AllCategory(category_name = item)
+            list.add(category)
+        }
+        categoryAdapter = CategoryAdapter(list, this)
+        binding.myRv.adapter = categoryAdapter
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ThirdFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ThirdFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onClick(allCategory: AllCategory, position: Int) {
+        allCategory.isSelected = !allCategory.isSelected
+        categoryAdapter.notifyItemChanged(position)
     }
 }
