d428258cbba63a923c186c9743a3c4bd50bf7f63
Saydullo <abdurashidovsaydullo35@gmail.com>
2023-04-13 17:10:32 +0500

    Dark Mode is find

--- app/src/main/java/uz/ilhomjon/newsapp/view/ui/SettingsFragment.kt
+++ app/src/main/java/uz/ilhomjon/newsapp/view/ui/SettingsFragment.kt
@@ -5,10 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import uz.ilhomjon.newsapp.R
 import uz.ilhomjon.newsapp.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    }
 
     private val binding by lazy { FragmentSettingsBinding.inflate(layoutInflater) }
     override fun onCreateView(
