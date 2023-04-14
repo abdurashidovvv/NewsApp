222995ed0525dd582c1b90ea712070b07f039f1a
Saydullo <abdurashidovsaydullo35@gmail.com>
2023-04-14 16:28:15 +0500

    Add Language and Info Fragments

---
+++ app/src/main/java/uz/ilhomjon/newsapp/view/ui/LanguageFragment.kt
@@ -0,0 +1,23 @@
+package uz.ilhomjon.newsapp.view.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import uz.ilhomjon.newsapp.R
+import uz.ilhomjon.newsapp.databinding.FragmentLanguageBinding
+
+
+class LanguageFragment : Fragment() {
+
+
+    private val binding by lazy { FragmentLanguageBinding.inflate(layoutInflater) }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+
+        return binding.root
+    }
+}
