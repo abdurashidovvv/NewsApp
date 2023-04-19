542c3fe57dd1962f61a338392548f33a21faf8a2
Saydullo <abdurashidovsaydullo35@gmail.com>
2023-04-19 13:51:14 +0500

    Info Fragment Finished.!

--- app/src/main/java/uz/ilhomjon/newsapp/view/ui/InfoFragment.kt
+++ app/src/main/java/uz/ilhomjon/newsapp/view/ui/InfoFragment.kt
@@ -11,23 +11,24 @@ import uz.ilhomjon.newsapp.databinding.FragmentInfoBinding
 import uz.ilhomjon.newsapp.models.TopHeadlines.Article
 
 @Suppress("DEPRECATION")
-class   InfoFragment : Fragment() {
+class InfoFragment : Fragment() {
 
-    private val binding by lazy { FragmentInfoBinding.inflate(layoutInflater)}
+    private val binding by lazy { FragmentInfoBinding.inflate(layoutInflater) }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
 
-        val article=arguments?.getSerializable("article") as Article
+        val article = arguments?.getSerializable("article") as Article
 
-        binding.title.text=article.title
-        binding.infoTv.text=article.description
+        binding.title.text = article.title
+        binding.infoTv.text = article.description
         Picasso.get().load(article.urlToImage).into(binding.image)
 
         binding.back.setOnClickListener {
             findNavController().popBackStack()
         }
+
         return binding.root
     }
 }
