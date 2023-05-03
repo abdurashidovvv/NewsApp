323f694b6561d014745b0f4a93a6e3555d7a60af
Saydullo <abdurashidovsaydullo35@gmail.com>
2023-05-03 18:58:57 +0500

    Finished Application.!

--- app/src/main/java/uz/ilhomjon/newsapp/view/ui/SplashScreenFragment.kt
+++ app/src/main/java/uz/ilhomjon/newsapp/view/ui/SplashScreenFragment.kt
@@ -4,20 +4,26 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import uz.ilhomjon.newsapp.App
 import uz.ilhomjon.newsapp.R
 import uz.ilhomjon.newsapp.databinding.FragmentSplashScreenBinding
 import uz.ilhomjon.newsapp.viewmodel.DatabaseViewModel
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 
 @SuppressLint("CustomSplashScreen")
-class SplashScreenFragment : Fragment() {
+class SplashScreenFragment : Fragment(), CoroutineScope {
 
     @Inject
     lateinit var databaseViewModel: DatabaseViewModel
@@ -33,15 +39,23 @@ class SplashScreenFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
 
-        val db = databaseViewModel.getAllCategory()
+
         var status: Boolean? = null
-        status = db.value.data != null
+        launch {
+            val db = databaseViewModel.getAllCategory().collectLatest {
+                    Log.d("@splashScreen", "onCreateView: ${it.data}")
+
+                status = it.data?.size != 0
+                Log.d("@splashScreen", "onCreateView: $status")
+            }
+
+        }
 
         val handler = Handler(Looper.getMainLooper())
         handler.postDelayed({
-            if (status){
+            if (status == true) {
                 findNavController().navigate(R.id.mainFragment)
-            }else{
+            } else {
                 findNavController().navigate(R.id.firstFragment)
             }
         }, 3000)
@@ -49,4 +63,7 @@ class SplashScreenFragment : Fragment() {
 
         return binding.root
     }
+
+    override val coroutineContext: CoroutineContext
+        get() = Job()
 }
